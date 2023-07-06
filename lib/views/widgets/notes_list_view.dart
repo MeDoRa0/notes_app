6b0b426cab7bca9263a7c718b956ecbd045dbd4b
MeDoRa0 <mohamed.hossam93x@gmail.com>
2023-07-06 21:25:27 +0300

    creat show model botom sheet

--- lib/views/widgets/notes_list_view.dart
+++ lib/views/widgets/notes_list_view.dart
@@ -5,16 +5,21 @@ import 'custom_note_item.dart';
 
 class NotesListView extends StatelessWidget {
   const NotesListView({super.key}); 
-  
+
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemBuilder: (context, index) {
-        return const Padding(
-          padding: EdgeInsets.symmetric(vertical: 6),
-          child: NoteItem(),
-        );
-      },
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 16),
+      child: ListView.builder(
+        //to remove the defult padding of listview
+        padding: EdgeInsets.zero,
+        itemBuilder: (context, index) {
+          return const Padding(
+            padding: EdgeInsets.symmetric(vertical: 6),
+            child: NoteItem(),
+          );
+        },
+      ),
     );
   }
 }
