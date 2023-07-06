3dea6e5bf2d75d2024ae7034fad01b0b75388ecc
MeDoRa0 <mohamed.hossam93x@gmail.com>
2023-07-06 19:37:16 +0300

    creat notes list view

---
+++ lib/views/widgets/notes_list_view.dart
@@ -0,0 +1,20 @@
+// noteslistview to show list of notes
+import 'package:flutter/material.dart';
+
+import 'custom_note_item.dart';
+
+class NotesListView extends StatelessWidget {
+  const NotesListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemBuilder: (context, index) {
+        return const Padding(
+          padding: EdgeInsets.symmetric(vertical: 8),
+          child: NoteItem(),
+        );
+      },
+    );
+  }
+}
--- lib/views/widgets/notes_view_body.dart
+++ lib/views/widgets/notes_view_body.dart
@@ -1,25 +1,27 @@
 import 'package:flutter/material.dart';
 
 import 'custom_app_bar.dart';
-import 'custom_note_item.dart';
+import 'notes_list_view.dart';
 
 class NotesViewBody extends StatelessWidget {
   const NotesViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 20),
+    return  Padding(
+      padding:  const EdgeInsets.symmetric(horizontal: 20),
       child: Column(
-        children: const [
+        children: [
           SizedBox(
             height: 60,
           ),
+          //calling CustomAPPBar
           CustomAppBar(),
-          NoteItem()
+          //calling NotesListView
+          //we warp with expaned to make all notes appear in screen
+          Expanded(child: NotesListView()),
         ],
       ),
     );
   }
 }
-
