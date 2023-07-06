3281bbde91adb08ab7ee4e56d1e97247d245df73
MeDoRa0 <mohamed.hossam93x@gmail.com>
2023-07-06 17:23:39 +0300

    creat custom note item

---
+++ lib/views/widgets/custom_note_item.dart
@@ -0,0 +1,61 @@
+// building note item
+import 'package:flutter/material.dart';
+
+class NoteItem extends StatelessWidget {
+  const NoteItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.only(top: 24, bottom: 24, left: 16),
+      decoration: BoxDecoration(
+        color: Color.fromARGB(255, 237, 223, 98),
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          ListTile(
+            title: Text(
+              "first note",
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 26,
+              ),
+            ),
+            subtitle: Padding(
+              padding: const EdgeInsets.only(
+                top: 16,
+                bottom: 16,
+              ),
+              child: Text(
+                "first note by mohamed hossam",
+                style: TextStyle(
+                  color: Colors.black.withOpacity(.5),
+                  fontSize: 18,
+                ),
+              ),
+            ),
+            trailing: IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.delete,
+                  color: Colors.red,
+                  size: 24,
+                )),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(right: 24),
+            child: Text(
+              "July 6 , 2023",
+              style: TextStyle(
+                color: Colors.black.withOpacity(.4),
+                fontSize: 16,
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
--- lib/views/widgets/notes_view_body.dart
+++ lib/views/widgets/notes_view_body.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'custom_app_bar.dart';
+import 'custom_note_item.dart';
 
 class NotesViewBody extends StatelessWidget {
   const NotesViewBody({super.key});
@@ -22,48 +23,3 @@ class NotesViewBody extends StatelessWidget {
   }
 }
 
-// building note item
-class NoteItem extends StatelessWidget {
-  const NoteItem({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: Color.fromARGB(255, 237, 223, 98),
-        borderRadius: BorderRadius.circular(16),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          ListTile(
-            title: Text(
-              "first note",
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 26,
-              ),
-            ),
-            subtitle: Text(
-              "first note by mohamed hossam",
-              style: TextStyle(
-                color: Colors.black.withOpacity(.4),
-                fontSize: 20,
-              ),
-            ),
-            trailing: IconButton(
-                onPressed: () {},
-                icon: Icon(
-                  Icons.delete,
-                  color: Colors.red,
-                )),
-          ),
-          Text(
-            "July 6 , 2023",
-            style: TextStyle(color: Colors.black),
-          )
-        ],
-      ),
-    );
-  }
-}
