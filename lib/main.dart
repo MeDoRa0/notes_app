e089d096b32498876744c00e9ab4d32234c17630
MeDoRa0 <mohamed.hossam93x@gmail.com>
2023-07-12 00:45:35 +0300

    register adabter

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:notes_app/constant.dart';
+import 'package:notes_app/models/note_model.dart';
 import 'package:notes_app/views/notes_view.dart';
 
 void main() async {
   //to intiate hive
   await Hive.initFlutter();
   //use await because openbox is future, open note box
- await Hive.openBox(kNotesBox);
+  await Hive.openBox(kNotesBox);
+  //to tell hive to deal with notemodel by  register type adapter named NoteModelAdabter that created in note_model.g.dart
+  Hive.registerAdapter(NoteModelAdapter());
   runApp(const NotesApp());
 }
 
