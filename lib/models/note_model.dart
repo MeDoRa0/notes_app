ce3c34f33e80651fe44a21b86f1b53fa76be0981
MeDoRa0 <mohamed.hossam93x@gmail.com>
2023-07-11 21:50:06 +0300

    generate file adabter for model, detrmine type id

--- lib/models/note_model.dart
+++ lib/models/note_model.dart
@@ -1,7 +1,19 @@
-class NoteModel {
+import 'package:hive/hive.dart';
+//this fill will be generated to creat file adabter for this model
+part 'note_model.g.dart';
+
+// determine type id for the object in hive database
+@HiveType(typeId: 0)
+// we use hiveobject to make the model able to store in hive
+class NoteModel extends HiveObject {
+  //give each field a hivefield id
+  @HiveField(0)
   final String title;
+  @HiveField(1)
   final String subTitle;
+  @HiveField(2)
   final String date;
+  @HiveField(3)
   final int color;
 
   NoteModel(
